3ced07174448e6e80ceb86cf0fa0f3d4c43a04c6
LEMSantos <lucas_m-santos@hotmail.com>
2021-04-25 17:59:06 -0300

    Add - verifica se o usuário está ativado ao fazer login

--- resources/user.py
+++ resources/user.py
@@ -18,6 +18,10 @@ USER_DELETED = 'User deleted.'
 USER_CREATED = 'User created successfully.'
 INVALID_CREDENTIALS = 'Invalid credentials.'
 USER_LOGGED_OUT = 'User <id={id}> successfully logged out.'
+NOT_CONFIRMED_ERROR = (
+    'You have not confirmed registration, please check your email <{}>.'
+)
+USER_CONFIRMED = 'User confirmed.'
 
 user_schema = UserSchema()
 
@@ -64,12 +68,18 @@ class UserLogin(Resource):
         user = UserModel.find_by_username(user_data.username)
 
         if user and safe_str_cmp(user.password, user_data.password):
-            access_token = create_access_token(identity=user.id, fresh=True)
-            refresh_token = create_refresh_token(user.id)
-            return {
-                'access_token': access_token,
-                'refresh_token': refresh_token,
-            }, 200
+            if user.activated:
+                access_token = create_access_token(
+                    identity=user.id, fresh=True
+                )
+                refresh_token = create_refresh_token(user.id)
+
+                return {
+                    'access_token': access_token,
+                    'refresh_token': refresh_token,
+                }, 200
+
+            return {'message': NOT_CONFIRMED_ERROR.format(user.username)}, 400
 
         return {'message': INVALID_CREDENTIALS}, 401
 
