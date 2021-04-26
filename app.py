3227f4a8ca4e361ef41e8af1fa44a0df73f5a8d7
LEMSantos <lucas_m-santos@hotmail.com>
2021-04-25 21:20:08 -0300

    Add - endpoint para confirmação do usuário

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from resources.user import (
     User,
     TokenRefresh,
     UserLogout,
+    UserConfirm,
 )
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
@@ -56,6 +57,7 @@ api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(TokenRefresh, '/refresh')
 api.add_resource(UserLogout, '/logout')
+api.add_resource(UserConfirm, '/user_confirm/<int:user_id>')
 
 if __name__ == '__main__':
     db.init_app(app)
