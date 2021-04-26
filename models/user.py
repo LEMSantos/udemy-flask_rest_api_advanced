def7ff534f97b4bbf46637b924fb454e2544c786
LEMSantos <lucas_m-santos@hotmail.com>
2021-04-25 23:08:57 -0300

    Add - método de confirmação do email

--- models/user.py
+++ models/user.py
@@ -1,3 +1,5 @@
+from flask import request, url_for
+from requests import Response, post
 from db import db
 
 
@@ -7,6 +9,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
     activated = db.Column(db.Boolean, default=False)
 
     @classmethod
@@ -14,8 +17,29 @@ class UserModel(db.Model):
         return cls.query.filter_by(username=username).first()
 
     @classmethod
+    def find_by_email(cls, email: str) -> 'UserModel':
+        return cls.query.filter_by(email=email).first()
+
+    @classmethod
     def find_by_id(cls, _id: int) -> 'UserModel':
         return cls.query.filter_by(id=_id).first()
+
+    def send_confirmation_email(self) -> Response:
+        link = request.url_root[:-1] + url_for('userconfirm', user_id=self.id)
+
+        return post(
+            f'https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages',
+            auth=('api', MAILGUN_API_KEY),
+            data={
+                'from': f'{FROM_TITLE} <{FROM_EMAIL}>',
+                'to': self.email,
+                'subject': 'Registration confirmation',
+                'text': (
+                    'Please click the link to confirm '
+                    f'your registration: {link}'
+                ),
+            },
+        )
 
     def save_to_db(self) -> None:
         db.session.add(self)
