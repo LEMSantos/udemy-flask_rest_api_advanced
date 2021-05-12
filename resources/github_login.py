87b08a191ed0d796bcd5e79ba91e3d97cf1bab40
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-11 23:04:25 -0300

    Fix - transforma a url em algo mais genérico

--- resources/github_login.py
+++ resources/github_login.py
@@ -1,5 +1,4 @@
-from flask import g
-from flask import request
+from flask import g, request, url_for
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token, create_refresh_token
 from oa import github
@@ -10,9 +9,7 @@ from models.user import UserModel
 class GithubLogin(Resource):
     @classmethod
     def get(cls):
-        return github.authorize(
-            callback='http://localhost:5000/login/github/authorized'
-        )
+        return github.authorize(url_for('github.authorize', _external=True))
 
 
 class GithubAuthorize(Resource):
