f6da4be83839b95a312f9fd53e0401bbb17a468a
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-11 23:04:41 -0300

    Add - alias para a rota de autorização

--- app.py
+++ app.py
@@ -71,7 +71,9 @@ api.add_resource(AvatarUpload, '/upload/avatar')
 api.add_resource(Image, '/image/<string:filename>')
 api.add_resource(Avatar, '/avatar/<int:user_id>')
 api.add_resource(GithubLogin, '/login/github')
-api.add_resource(GithubAuthorize, '/login/github/authorized')
+api.add_resource(
+    GithubAuthorize, '/login/github/authorized', endpoint='github.authorize'
+)
 api.add_resource(SetPassword, '/user/password')
 
 db.init_app(app)
