be07d214b5b0b331662332779d0a777533b6ceb0
LEMSantos <lucas_m-santos@hotmail.com>
2021-05-14 22:08:43 -0300

    Add - lida com vários items iguais para uma mesma order

--- resources/order.py
+++ resources/order.py
@@ -1,10 +1,10 @@
+from collections import Counter
 from flask_restful import Resource
 from flask import request
 
 from libs.strings import gettext
 from models.item import ItemModel
-
-# from models.order import OrderModel
+from models.order import OrderModel, ItemsInOrder
 
 
 class Order(Resource):
@@ -15,9 +15,10 @@ class Order(Resource):
         """
         data = request.get_json()  # token + list of item ids
         items = []
+        item_id_quantities = Counter(data['item_ids'])
 
         # Iterate over items and retrive them from the database.
-        for _id in data['item_ids']:
+        for _id, count in item_id_quantities.most_common():
             item = ItemModel.find_by_id(_id)
 
             if not item:
@@ -27,7 +28,7 @@ class Order(Resource):
                     ),
                 }, 404
 
-            items.append(item)
+            items.append(ItemsInOrder(item_id=_id, quantity=count))
 
         order = OrderModel(items=items, status='pending')
         order.save_to_db()  # this does not submit to Stripe
